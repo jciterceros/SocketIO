33cf01e0326a5f5eb9f3a5ca2ba60a81bac51b0c
jciterceros <jciterceros@yahoo.com>
2021-09-27 01:21:08 -0300

    Example SocketIO

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/*
+import express from 'express';
+import socketio from 'socket.io';
+import http from 'http';
+
+import './index.css';
+import App from './App';
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new socketio.Server(httpServer);
+
+io.on('connection', (socket)=>{
+  console.log(`new connection: ${socket.id}`);
+})
+
+httpServer.listen(3000);
+*/
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
